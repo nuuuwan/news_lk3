7148f923dddaab60b3f359a3e15a16fd5054ca9c
nuuuwan <nuuuwan@gmail.com>
2022-11-20 09:34:43 +0530

    refac: -> news_lk3 -> news_lk3_data

--- src/news_lk3/_constants.py
+++ src/news_lk3/_constants.py
@@ -7,5 +7,5 @@ TEST_MODE = False
 WORDS_PER_MINUTE = 250
 
 URL_GITHUB_BASE = 'https://github.com/nuuuwan/news_lk3/blob/data'
-DIR_TMP_BASE = '/tmp/news_lk3'
+DIR_TMP_BASE = '/tmp/news_lk3_data'
 DELIM_MD = '\n' * 2
--- src/news_lk3/tests/test_filesys.py
+++ src/news_lk3/tests/test_filesys.py
@@ -7,7 +7,7 @@ class TestFileSys(unittest.TestCase):
     def test_get_dir_article_shard(self):
         TEST_ARTICLE_FILE_ONLY = '12345678.json'
         self.assertEqual(
-            '/tmp/news_lk3/articles/12',
+            '/tmp/news_lk3_data/articles/12',
             filesys.get_dir_article_shard(TEST_ARTICLE_FILE_ONLY),
         )
 
@@ -21,7 +21,7 @@ class TestFileSys(unittest.TestCase):
     def test_get_article_file(self):
         TEST_URL = 'https://www.google.com'
         self.assertEqual(
-            '/tmp/news_lk3/articles/fe/fe82f1d1.json',
+            '/tmp/news_lk3_data/articles/fe/fe82f1d1.json',
             filesys.get_article_file(TEST_URL),
         )
 
