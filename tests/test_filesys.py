769843c9a016cbf2a3238860ffc91af721b5446a
nuuuwan <nuuuwan@gmail.com>
2023-02-26 06:03:30 +0530

    fix: failing unittests

--- tests/test_filesys.py
+++ tests/test_filesys.py
@@ -7,14 +7,14 @@ class TestFileSys(unittest.TestCase):
     def test_get_article_file_only(self):
         TEST_URL = 'https://www.google.com'
         self.assertEqual(
-            'fe82f1d1.json',
+            'c724fbbd.json',
             filesys.get_article_file_only(TEST_URL),
         )
 
     def test_get_article_file(self):
         TEST_URL = 'https://www.google.com'
         self.assertEqual(
-            '/tmp/news_lk3_data/fe82f1d1.json',
+            '/tmp/news_lk3_data/c724fbbd.json',
             filesys.get_article_file(TEST_URL),
         )
 
