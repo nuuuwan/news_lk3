122a0a3cf93e4403d64dc76199f5fad53062f6d9
nuuuwan <nuuuwan@gmail.com>
2024-01-15 14:05:02 +0530

    Added newspaper stats

--- src/news_lk3/reports/ReadMe.py
+++ src/news_lk3/reports/ReadMe.py
@@ -15,10 +15,25 @@ class ReadMe(ArticleSummary):
     @staticmethod
     def render_article(article) -> list[str]:
         return [
-            f'## {article.original_title}',
+            f'### {article.original_title}',
             f'*{TIME_FORMAT_TIME.stringify(Time(article.time_ut))}'
             + f' - [{article.newspaper_id}]({article.url})*',
         ] + article.original_body_lines
+    
+    @staticmethod
+    def render_article_stats(article_list) -> list[str]:
+        lines = []
+        newspaper_to_n = {}
+        for article in article_list:
+            newspaper_to_n[article.newspaper_id] = (
+                newspaper_to_n.get(article.newspaper_id, 0) + 1
+            )
+        
+        lines.append('## Newspaper Stats')
+        for newspaper_id, n in newspaper_to_n.items():
+            lines.append(f'* {n:, }{newspaper_id}')
+        lines.append(f'* Total: {len(article_list):, }')
+        return lines
 
     def write(self):
         articles = self.articles
@@ -27,9 +42,12 @@ class ReadMe(ArticleSummary):
         )
 
         lines = [
-            f'# Articles (Latest {ReadMe.N_DISPLAY})',
+            f'# Newspaper Articles from Sri Lanka :lka:',
             f'As of **{TIME_FORMAT_TIME.stringify(Time.now())}**',
         ]
+        lines.extend(ReadMe.render_article_stats(sorted_articles))
+
+        lines.append(f'## Latest Articles ({ReadMe.N_DISPLAY})')
         for article in sorted_articles[: self.N_DISPLAY]:
             lines.extend(ReadMe.render_article(article))
 
