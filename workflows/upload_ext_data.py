425f51360ff0a5606bb31e4b67995f14790dc94f
nuuuwan <nuuuwan@gmail.com>
2024-01-20 17:01:33 +0530

    lint

--- workflows/upload_ext_data.py
+++ workflows/upload_ext_data.py
@@ -1,13 +1,15 @@
 import os
+import time
 
 from _upload_common import init_dir
-from utils import Log, SECONDS_IN
-import time
+from utils import SECONDS_IN, Log
+
 from news_lk3.core import Article, ExtArticle
 
 log = Log('upload_ext_data')
 
 MAX_RUNNING_TIME_S = 1 * SECONDS_IN.MINUTE
+
 
 def main():
     t_start = time.time()
@@ -15,19 +17,19 @@ def main():
     articles = Article.list_from_remote()
     init_dir()
     for article in articles:
-        
+
         d_time = time.time() - t_start
         log.debug(f'{d_time=:.1f}s')
         if d_time > MAX_RUNNING_TIME_S:
             log.info(f'{d_time=:.1f}s > {MAX_RUNNING_TIME_S}s. Stopping.')
             break
 
-            
         ext_article = ExtArticle.from_article(article, force_extend=True)
         if os.path.exists(ext_article.file_name):
             log.debug(f'{ext_article.file_name} exists. Skipping.')
             continue
         ext_article.store()
-        
+
+
 if __name__ == '__main__':
     main()
