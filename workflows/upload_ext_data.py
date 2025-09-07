09af7c46a0ed7fcd8a001ffacff518e5609a6f32
nuuuwan <nuuuwan@gmail.com>
2025-09-07 10:39:59 +0530

    More TimeUnit BUGS

--- workflows/upload_ext_data.py
+++ workflows/upload_ext_data.py
@@ -1,17 +1,17 @@
 import os
 import time
 
-from utils import SECONDS_IN, Log
+from utils import Log, TimeUnit
 
 from news_lk3.core import Article, ExtArticle
 
-log = Log('upload_ext_data')
+log = Log("upload_ext_data")
 
-TEST_MODE = os.name == 'nt'
-log.debug(f'{TEST_MODE=}')
+TEST_MODE = os.name == "nt"
+log.debug(f"{TEST_MODE=}")
 MAX_RUNNING_TIME_M = 0.1 if TEST_MODE else 10
-MAX_RUNNING_TIME_S = MAX_RUNNING_TIME_M * SECONDS_IN.MINUTE
-log.debug(f'{MAX_RUNNING_TIME_S=}')
+MAX_RUNNING_TIME_S = MAX_RUNNING_TIME_M * TimeUnit.SECONDS_IN.MINUTE
+log.debug(f"{MAX_RUNNING_TIME_S=}")
 
 
 def main():
@@ -20,19 +20,17 @@ def main():
 
     for article in articles:
         d_time = time.time() - t_start
-        log.debug(f'{d_time=:.1f}s')
+        log.debug(f"{d_time=:.1f}s")
         if d_time > MAX_RUNNING_TIME_S:
-            log.info(f'{d_time=:.1f}s > {MAX_RUNNING_TIME_S}s. Stopping.')
+            log.info(f"{d_time=:.1f}s > {MAX_RUNNING_TIME_S}s. Stopping.")
             break
 
         ea = ExtArticle.from_article(article, force_extend=True)
         if os.path.exists(ea.relative_ext_article_file_path):
-            log.debug(
-                f'{ea.relative_ext_article_file_path} exists. Skipping.'
-            )
+            log.debug(f"{ea.relative_ext_article_file_path} exists. Skipping.")
             continue
         ea.store()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
