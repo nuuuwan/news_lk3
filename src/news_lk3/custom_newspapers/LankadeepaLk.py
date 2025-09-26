15aaa7f125b723518cf86db9ccfd69bb6701cfe5
nuuuwan <nuuuwan@gmail.com>
2025-09-26 13:07:20 +0530

    FIX LankadeepaLk

--- src/news_lk3/custom_newspapers/LankadeepaLk.py
+++ src/news_lk3/custom_newspapers/LankadeepaLk.py
@@ -4,36 +4,38 @@ from news_lk3.core import AbstractNewsPaper
 class LankadeepaLk(AbstractNewsPaper):
     @classmethod
     def get_original_lang(cls):
-        return 'si'
+        return "si"
 
     @classmethod
     def get_index_urls(cls):
         return [
-            'https://www.lankadeepa.lk/latest_news/1',
-            'https://www.lankadeepa.lk/feature/2',
-            'https://www.lankadeepa.lk/politics/13',
-            'https://www.lankadeepa.lk/editorial/117',
+            "https://www.lankadeepa.lk/latest_news/1",
+            "https://www.lankadeepa.lk/feature/2",
+            "https://www.lankadeepa.lk/politics/13",
+            "https://www.lankadeepa.lk/editorial/117",
         ]
 
     @classmethod
     def parse_article_urls(cls, soup):
         article_urls = []
-        for h3 in soup.find_all('h3', {'class': 'mtbfive'}):
-            article_url = h3.find('a').get('href')
+        for h5 in soup.find_all("h5", {"class": "p-b-0"}):
+            article_url = h5.find("a").get("href")
             article_urls.append(article_url)
         return article_urls
 
     @classmethod
     def parse_title(cls, soup):
-        h1 = soup.find('h1', {'class': 'post-title'})
-        return h1.text
+        h3 = soup.find("h3", {"class": "f1-l-3 cl2 p-b-0 respon2"})
+        assert h3, "[parse_title] no h3"
+        return h3.text
 
     @classmethod
     def parse_body_lines(cls, soup):
-        header_inner = soup.find('header', {'class': 'post-content'})
+        div_body = soup.find("div", {"class": "header inner-content p-b-20"})
+        assert div_body, "[parse_body_lines] no header_inner"
         return list(
             map(
                 lambda line: line.strip(),
-                header_inner.text.strip().split('\n'),
+                div_body.text.strip().split("\n"),
             )
         )
