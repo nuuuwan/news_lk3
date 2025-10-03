38fe462a134c8ad32e596928e7ceac859cf2e2ad
nuuuwan <nuuuwan@gmail.com>
2025-10-03 12:28:33 +0530

    BUGFIX NewspaperFactory should import classes not modules

--- src/news_lk3/custom_newspapers/NewspaperFactory.py
+++ src/news_lk3/custom_newspapers/NewspaperFactory.py
@@ -1,11 +1,20 @@
-from news_lk3.custom_newspapers import (AdaDeranaLk, AdaDeranaSinhalaLk, AdaLk,
-                                        BBCComSinhala, CeylonTodayLk,
-                                        ColomboTelegraphCom, DailyFtLk,
-                                        DailyMirrorLk, DailyNewsLk,
-                                        DBSJeyarajCom, DivainaLk,
-                                        EconomyNextCom, IslandLk, LankadeepaLk,
-                                        NewsFirstLk, TamilMirrorLk,
-                                        VirakesariLk)
+from news_lk3.custom_newspapers.AdaDeranaLk import AdaDeranaLk
+from news_lk3.custom_newspapers.AdaDeranaSinhalaLk import AdaDeranaSinhalaLk
+from news_lk3.custom_newspapers.AdaLk import AdaLk
+from news_lk3.custom_newspapers.BBCComSinhala import BBCComSinhala
+from news_lk3.custom_newspapers.CeylonTodayLk import CeylonTodayLk
+from news_lk3.custom_newspapers.ColomboTelegraphCom import ColomboTelegraphCom
+from news_lk3.custom_newspapers.DailyFtLk import DailyFtLk
+from news_lk3.custom_newspapers.DailyMirrorLk import DailyMirrorLk
+from news_lk3.custom_newspapers.DailyNewsLk import DailyNewsLk
+from news_lk3.custom_newspapers.DBSJeyarajCom import DBSJeyarajCom
+from news_lk3.custom_newspapers.DivainaLk import DivainaLk
+from news_lk3.custom_newspapers.EconomyNextCom import EconomyNextCom
+from news_lk3.custom_newspapers.IslandLk import IslandLk
+from news_lk3.custom_newspapers.LankadeepaLk import LankadeepaLk
+from news_lk3.custom_newspapers.NewsFirstLk import NewsFirstLk
+from news_lk3.custom_newspapers.TamilMirrorLk import TamilMirrorLk
+from news_lk3.custom_newspapers.VirakesariLk import VirakesariLk
 
 
 class NewspaperFactory:
